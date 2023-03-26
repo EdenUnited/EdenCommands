1aa6e9b27c2055a64eb63f4dcb870a803ad3eaba
Haha007 <nichtechtanonym@gmail.com>
2023-03-26 22:56:25 +0200

    Add Annotations to README, sort DefaultArgumentParsers

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A command library to register Commands in a tree-style fashion.
 ## Usage
 
 ### Nodes
-CommandNodes are a tree based system.  
-There are 2 basic variations of CommandNodes, the LiteralCommandNode and the ArgumentCommandNode.  
+CommandNodes are a tree based system.
+There are 2 basic variations of CommandNodes, the LiteralCommandNode and the ArgumentCommandNode.
 The root node has to be a `LiteralCommandNode`.
 
 #### Literals
@@ -56,6 +56,64 @@ class MyPlugin extends JavaPlugin {
         node.then(argument);
 
         registry.register(node);
+    }
+}
+```
+
+### Annotations
+
+
+The `@Command("mycommand")` annotation on the class can be used as a prefix, but is optional.
+Just load all classes with command executors with the `AnnotationCommandLoader` and register them with the `CommandRegistry`.
+You have to add the parsers for the arguments you want to use with `AnnotationCommandLoader#addArgumentParser`,
+the basics are included using the `AnnotationCommandLoader#addDefaultArgumentParsers` (boolean, double, float, int, long, offline_player, player, string).
+
+
+```java
+@Command("mycommand")
+class MyPlugin extends JavaPlugin {
+    public void onEnable() {
+        CommandRegistry registry = new CommandRegistry(this);
+        AnnotatedCommandLoader loader = new AnnotatedCommandLoader(this);
+        loader.addDefaultArgumentParsers();
+        loader.addArgumentParser("item", EnumArgumentParser.itemMaterialParser());
+        loader.addAnnotated(this);
+        loader.register(registry);
+    }
+
+    // -> /mycommand subcommand
+    @Command("subcommand")
+    public void subcommand(CommandContext context) {
+        context.sender().sendMessage("subcommand executed");
+    }
+
+    // -> /mycommand setblock
+    @Command(value = "setblock", sync = true)
+    public void subcommand(CommandContext context) {
+        if (!(context.sender() instanceof Player player)) {
+            context.sender().sendMessage("You must be a player to use this command.");
+            return;
+        }
+        Random random = new Random();
+        List<Material> list = Arrays.stream(Material.values()).filter(Material::isBlock).toList();
+        Material block = list.get(random.nextInt(list.size()));
+        player.getLocation().getBlock().setType(block);
+    }
+
+    // -> /mycommand stick
+    @Command("item{type:item}")
+    public void item(CommandContext context) {
+        Material material = context.parameter("item");
+        context.sender().sendMessage(material.toString());
+    }
+
+    // -> /mycommand Haha007 stick 64
+    @Command("player{type:player} item{type:item} amount{type:int,range:'1,64',suggest:'1,8,16,32,64'}")
+    public void item(CommandContext context) {
+        Player player = context.parameter("player");
+        Material material = context.parameter("item");
+        int amount = context.parameter("amount");
+        player.getInventory().addItem(new ItemStack(material, amount));
     }
 }
 ```
--- src/main/java/at/haha007/edencommands/annotations/DefaultArgumentParsers.java
+++ src/main/java/at/haha007/edencommands/annotations/DefaultArgumentParsers.java
@@ -11,109 +11,10 @@ import net.kyori.adventure.util.TriState;
 import java.util.Map;
 
 public enum DefaultArgumentParsers implements ArgumentParser<Argument<?>> {
-    STRING("string") {
-        public Argument<?> parse(Map<String, String> params) {
-            StringArgument.StringArgumentBuilder builder = StringArgument.builder();
-            String parserKey = params.get("parser");
-            if (parserKey == null) parserKey = "word";
-            StringArgument.StringParser parser = switch (parserKey) {
-                case "word" -> StringArgument.word();
-                case "greedy" -> StringArgument.greedy();
-                case "quoted" -> {
-                    String error = params.getOrDefault("error", "Invalid quote usage!");
-                    yield StringArgument.quoted(mm.deserialize(error));
-                }
-                default -> throw new IllegalArgumentException("Unknown parser: " + parserKey);
-            };
-            builder.parser(parser);
-            params.entrySet().stream()
-                    .filter(e -> e.getKey()
-                            .startsWith("suggest"))
-                    .sorted(Map.Entry.comparingByKey())
-                    .map(Map.Entry::getValue)
-                    .map(AsyncTabCompleteEvent.Completion::completion)
-                    .forEach(builder::completion);
-            return builder.build();
-        }
-    },
-    LONG("long") {
-        public Argument<?> parse(Map<String, String> params) {
-            LongArgument.LongArgumentBuilder builder = LongArgument.builder();
-            if (params.containsKey("filter")) {
-                String filter = params.get("filter");
-                long min = Long.parseLong(filter.split(",")[0]);
-                long max = Long.parseLong(filter.split(",")[1]);
-                if (min > max) {
-                    long temp = min;
-                    min = max;
-                    max = temp;
-                }
-                String errorString = "Argument must be between " + min + " and " + max + "!";
-                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
-                builder.filter(new LongArgument.RangeFilter(error, min, max));
-            }
-            if (params.containsKey("suggest")) {
-                String[] suggest = params.get("suggest").split(",");
-                for (String s : suggest) {
-                    builder.completion(new Completion<>(Long.parseLong(s)));
-                }
-            }
-            String notLongMessage = params.getOrDefault("error", "Argument {0} must be of type long!");
-            builder.notLongMessage(c -> mm.deserialize(notLongMessage.replace("{0}", c)));
-            return builder.build();
-        }
-    },
-    INTEGER("int") {
-        public Argument<?> parse(Map<String, String> params) {
-            IntegerArgument.IntegerArgumentBuilder builder = IntegerArgument.builder();
-            if (params.containsKey("filter")) {
-                String filter = params.get("filter");
-                int min = Integer.parseInt(filter.split(",")[0]);
-                int max = Integer.parseInt(filter.split(",")[1]);
-                if (min > max) {
-                    int temp = min;
-                    min = max;
-                    max = temp;
-                }
-                String errorString = "Argument must be between " + min + " and " + max + "!";
-                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
-                builder.filter(new IntegerArgument.RangeFilter(error, min, max));
-            }
-            if (params.containsKey("suggest")) {
-                String[] suggest = params.get("suggest").split(",");
-                for (String ss : suggest) {
-                    builder.completion(new Completion<>(Integer.parseInt(ss)));
-                }
-            }
-            String notIntMessage = params.getOrDefault("error", "Argument {0} must be of type integer!");
-            builder.notIntegerMessage(c -> mm.deserialize(notIntMessage.replace("{0}", c)));
-            return builder.build();
-        }
-    },
-    FLOAT("float") {
-        public Argument<?> parse(Map<String, String> params) {
-            FloatArgument.FloatArgumentBuilder builder = FloatArgument.builder();
-            if (params.containsKey("filter")) {
-                String filter = params.get("filter");
-                float min = Float.parseFloat(filter.split(",")[0]);
-                float max = Float.parseFloat(filter.split(",")[1]);
-                if (min > max) {
-                    float temp = min;
-                    min = max;
-                    max = temp;
-                }
-                String errorString = "Argument must be between " + min + " and " + max + "!";
-                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
-                builder.filter(new FloatArgument.RangeFilter(error, min, max));
-            }
-            if (params.containsKey("suggest")) {
-                String[] suggest = params.get("suggest").split(",");
-                for (String ss : suggest) {
-                    builder.completion(new Completion<>(Float.parseFloat(ss)));
-                }
-            }
-            String notFloatMessage = params.getOrDefault("error", "Argument {0} must be of type float!");
-            builder.notFloatMessage(c -> mm.deserialize(notFloatMessage.replace("{0}", c)));
+    BOOLEAN("boolean") {
+        public Argument<?> parse(Map<String, String> s) {
+            BooleanArgument.BooleanArgumentBuilder builder = BooleanArgument.builder();
+            builder.notBooleanMessage(c -> mm.deserialize(s.getOrDefault("error", "Argument " + c + " must be a boolean!")));
             return builder.build();
         }
     },
@@ -144,10 +45,91 @@ public enum DefaultArgumentParsers implements ArgumentParser<Argument<?>> {
             return builder.build();
         }
     },
-    BOOLEAN("boolean") {
+    FLOAT("float") {
+        public Argument<?> parse(Map<String, String> params) {
+            FloatArgument.FloatArgumentBuilder builder = FloatArgument.builder();
+            if (params.containsKey("filter")) {
+                String filter = params.get("filter");
+                float min = Float.parseFloat(filter.split(",")[0]);
+                float max = Float.parseFloat(filter.split(",")[1]);
+                if (min > max) {
+                    float temp = min;
+                    min = max;
+                    max = temp;
+                }
+                String errorString = "Argument must be between " + min + " and " + max + "!";
+                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
+                builder.filter(new FloatArgument.RangeFilter(error, min, max));
+            }
+            if (params.containsKey("suggest")) {
+                String[] suggest = params.get("suggest").split(",");
+                for (String ss : suggest) {
+                    builder.completion(new Completion<>(Float.parseFloat(ss)));
+                }
+            }
+            String notFloatMessage = params.getOrDefault("error", "Argument {0} must be of type float!");
+            builder.notFloatMessage(c -> mm.deserialize(notFloatMessage.replace("{0}", c)));
+            return builder.build();
+        }
+    },
+    INTEGER("int") {
+        public Argument<?> parse(Map<String, String> params) {
+            IntegerArgument.IntegerArgumentBuilder builder = IntegerArgument.builder();
+            if (params.containsKey("filter")) {
+                String filter = params.get("filter");
+                int min = Integer.parseInt(filter.split(",")[0]);
+                int max = Integer.parseInt(filter.split(",")[1]);
+                if (min > max) {
+                    int temp = min;
+                    min = max;
+                    max = temp;
+                }
+                String errorString = "Argument must be between " + min + " and " + max + "!";
+                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
+                builder.filter(new IntegerArgument.RangeFilter(error, min, max));
+            }
+            if (params.containsKey("suggest")) {
+                String[] suggest = params.get("suggest").split(",");
+                for (String ss : suggest) {
+                    builder.completion(new Completion<>(Integer.parseInt(ss)));
+                }
+            }
+            String notIntMessage = params.getOrDefault("error", "Argument {0} must be of type integer!");
+            builder.notIntegerMessage(c -> mm.deserialize(notIntMessage.replace("{0}", c)));
+            return builder.build();
+        }
+    },
+    LONG("long") {
+        public Argument<?> parse(Map<String, String> params) {
+            LongArgument.LongArgumentBuilder builder = LongArgument.builder();
+            if (params.containsKey("filter")) {
+                String filter = params.get("filter");
+                long min = Long.parseLong(filter.split(",")[0]);
+                long max = Long.parseLong(filter.split(",")[1]);
+                if (min > max) {
+                    long temp = min;
+                    min = max;
+                    max = temp;
+                }
+                String errorString = "Argument must be between " + min + " and " + max + "!";
+                Component error = mm.deserialize(params.getOrDefault("filter_message", errorString));
+                builder.filter(new LongArgument.RangeFilter(error, min, max));
+            }
+            if (params.containsKey("suggest")) {
+                String[] suggest = params.get("suggest").split(",");
+                for (String s : suggest) {
+                    builder.completion(new Completion<>(Long.parseLong(s)));
+                }
+            }
+            String notLongMessage = params.getOrDefault("error", "Argument {0} must be of type long!");
+            builder.notLongMessage(c -> mm.deserialize(notLongMessage.replace("{0}", c)));
+            return builder.build();
+        }
+    },
+    OFFLINE_PLAYER("offline_player") {
         public Argument<?> parse(Map<String, String> s) {
-            BooleanArgument.BooleanArgumentBuilder builder = BooleanArgument.builder();
-            builder.notBooleanMessage(c -> mm.deserialize(s.getOrDefault("error", "Argument " + c + " must be a boolean!")));
+            OfflinePlayerArgument.OfflinePlayerArgumentBuilder builder = OfflinePlayerArgument.builder();
+            builder.playerNotFoundErrorProvider(c -> mm.deserialize(s.getOrDefault("error", "Player " + c + " not found!")));
             return builder.build();
         }
     },
@@ -159,10 +141,28 @@ public enum DefaultArgumentParsers implements ArgumentParser<Argument<?>> {
             return builder.build();
         }
     },
-    OFFLINE_PLAYER("offline_player") {
-        public Argument<?> parse(Map<String, String> s) {
-            OfflinePlayerArgument.OfflinePlayerArgumentBuilder builder = OfflinePlayerArgument.builder();
-            builder.playerNotFoundErrorProvider(c -> mm.deserialize(s.getOrDefault("error", "Player " + c + " not found!")));
+    STRING("string") {
+        public Argument<?> parse(Map<String, String> params) {
+            StringArgument.StringArgumentBuilder builder = StringArgument.builder();
+            String parserKey = params.get("parser");
+            if (parserKey == null) parserKey = "word";
+            StringArgument.StringParser parser = switch (parserKey) {
+                case "word" -> StringArgument.word();
+                case "greedy" -> StringArgument.greedy();
+                case "quoted" -> {
+                    String error = params.getOrDefault("error", "Invalid quote usage!");
+                    yield StringArgument.quoted(mm.deserialize(error));
+                }
+                default -> throw new IllegalArgumentException("Unknown parser: " + parserKey);
+            };
+            builder.parser(parser);
+            params.entrySet().stream()
+                    .filter(e -> e.getKey()
+                            .startsWith("suggest"))
+                    .sorted(Map.Entry.comparingByKey())
+                    .map(Map.Entry::getValue)
+                    .map(AsyncTabCompleteEvent.Completion::completion)
+                    .forEach(builder::completion);
             return builder.build();
         }
     };
